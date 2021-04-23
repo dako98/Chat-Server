e8bc20b4ead6c6b6ff72226a0dbfdf9f8af19526
Dako Dimov <dako98@gmail.com>
2021-04-23 17:19:10 +0300

    getUser uses reference for name now

--- src/UserStore.cpp
+++ src/UserStore.cpp
@@ -10,13 +10,19 @@ bool UserStore::addUser(const User &newUser)
     bool exists = users[newUser.getName()].getName() != "";
 
     if (!exists)
-    {   // FIXME: Make so we take the copy and move it to the map
+    { // FIXME: Make so we take the copy and move it to the map
         users[newUser.getName()] = newUser;
     }
     return !exists;
 }
 
-User& UserStore::getUser(const std::string name)
+/**
+ * @brief gives the user object for editing
+ * 
+ * @param name user from database
+ * @return User& 
+ */
+User &UserStore::getUser(const std::string &name)
 {
     return users[name];
 }
